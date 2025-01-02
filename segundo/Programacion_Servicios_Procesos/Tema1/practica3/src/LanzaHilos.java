11df84add53eff006115cfa7202a2c0dac3c1f96
walternoguera <walter.morel.noguera@gmail.com>
2025-01-02 16:11:51 +0100

    prueba de hilo y multiproceso

--- segundo/Programacion_Servicios_Procesos/Tema1/practica3/src/LanzaHilos.java
+++ segundo/Programacion_Servicios_Procesos/Tema1/practica3/src/LanzaHilos.java
@@ -14,14 +14,23 @@ public class LanzaHilos {
             List<Process> listaProceso = new ArrayList<>();
 
             for (String[] param : parametros) {
-                ProcessBuilder pb = new ProcessBuilder("java", "-cp", "C:\\Users\\WALTERALFONSOMORELNO\\Documents\\dam\\segundo\\Programacion_Servicios_Procesos\\Tema1\\practica2\\bin", "Caracol", param[0], param[1]);//reemplazar el path por bin
+                ProcessBuilder pb = new ProcessBuilder("java", "-cp",
+                        "C:\\Users\\WALTERALFONSOMORELNO\\Documents\\dam\\segundo\\Programacion_Servicios_Procesos\\Tema1\\practica2\\bin",
+                        "Caracol", param[0], param[1]);// reemplazar el path por bin
                 Process p = pb.start();
                 listaProceso.add(p);
-                BufferedReader salidaCaracol1 = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String linea;
-                while ((linea = salidaCaracol1.readLine()) != null) {
-                    System.out.println(linea);
-                }
+
+                new Thread(() -> { // clase thread
+                    try {
+                        BufferedReader salidaCaracol1 = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                        String linea;
+                        while ((linea = salidaCaracol1.readLine()) != null) {
+                            System.out.println(linea);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }).start();
             }
         } catch (Exception ee) {
             System.err.println("Exception: " + ee.getMessage());
