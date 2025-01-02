8aa15269f38a13a45014041c596be12efca54501
walternoguera <walter.morel.noguera@gmail.com>
2025-01-02 10:59:15 +0100

    modificación del path

--- segundo/Programacion_Servicios_Procesos/Tema1/practica2/src/LanzaCaracoles.java
+++ segundo/Programacion_Servicios_Procesos/Tema1/practica2/src/LanzaCaracoles.java
@@ -15,7 +15,7 @@ public class LanzaCaracoles {
             List<Process> listaProceso = new ArrayList<>();
 
             for (String[] param : parametros) {
-                ProcessBuilder pb = new ProcessBuilder("java", "-cp", "bin", "Caracol", param[0], param[1]);
+                ProcessBuilder pb = new ProcessBuilder("java", "-cp", "C:\\Users\\WALTERALFONSOMORELNO\\Documents\\dam\\segundo\\Programacion_Servicios_Procesos\\Tema1\\practica2\\bin", "Caracol", param[0], param[1]);//reemplazar el path por bin
                 Process p = pb.start();
                 listaProceso.add(p);
                 BufferedReader salidaCaracol1 = new BufferedReader(new InputStreamReader(p.getInputStream()));
