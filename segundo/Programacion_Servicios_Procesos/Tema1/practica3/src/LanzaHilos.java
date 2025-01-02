ea17450bfad9e3f79addd2de1182533ee8cf449c
walternoguera <walter.morel.noguera@gmail.com>
2025-01-02 13:41:40 +0100

    Terminamos gestión de procesos con caracoles, vemos un poco de hilos hacemos debug

---
+++ segundo/Programacion_Servicios_Procesos/Tema1/practica3/src/LanzaHilos.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+
+public class LanzaHilos {
+    public static void main(String[] args) {
+        String[][] parametros = {
+                { "3", "C" },
+                { "1", "D" }
+        };
+
+        try {
+            List<Process> listaProceso = new ArrayList<>();
+
+            for (String[] param : parametros) {
+                ProcessBuilder pb = new ProcessBuilder("java", "-cp", "C:\\Users\\WALTERALFONSOMORELNO\\Documents\\dam\\segundo\\Programacion_Servicios_Procesos\\Tema1\\practica2\\bin", "Caracol", param[0], param[1]);//reemplazar el path por bin
+                Process p = pb.start();
+                listaProceso.add(p);
+                BufferedReader salidaCaracol1 = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String linea;
+                while ((linea = salidaCaracol1.readLine()) != null) {
+                    System.out.println(linea);
+                }
+            }
+        } catch (Exception ee) {
+            System.err.println("Exception: " + ee.getMessage());
+        }
+    }
+}
